4ea97823937d275c39013b60eb4b35e27d5df2af
Brandon <43607839+brann0n@users.noreply.github.com>
2021-12-20 15:05:14 +0100

    Added cool feature with a controller interface

--- src/main/java/com/nhlstenden/student/vigmo/controllers/ScreenController.java
+++ src/main/java/com/nhlstenden/student/vigmo/controllers/ScreenController.java
@@ -5,22 +5,46 @@ import com.nhlstenden.student.vigmo.models.Screen;
 import com.nhlstenden.student.vigmo.services.ScreenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("screens")
-public class ScreenController {
-    private ScreenService screenService;
+public class ScreenController implements VigmoController<ScreenDto> {
+    private final ScreenService screenService;
 
-    public ScreenController(ScreenService service){
+    public ScreenController(ScreenService service) {
         this.screenService = service;
     }
 
-    @GetMapping
-    public ResponseEntity<List<ScreenDto>> get(){
+    @Override
+    public ResponseEntity<List<ScreenDto>> get() {
         return ResponseEntity.ok(screenService.getScreens());
+    }
+
+    @Override
+    public ResponseEntity<ScreenDto> get(final long id) {
+        return ResponseEntity.ok(screenService.getScreen(id));
+    }
+
+    @Override
+    public ResponseEntity<Void> post(ScreenDto postObject) {
+        return ResponseEntity.created(URI.create(String.format("/screens/%d", screenService.createScreen(postObject)))).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> put(final long id, ScreenDto putObject) {
+        screenService.updateScreen(putObject, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(final long id) {
+        screenService.deleteScreen(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/nhlstenden/student/vigmo/repositories/ScreenRepository.java
+++ src/main/java/com/nhlstenden/student/vigmo/repositories/ScreenRepository.java
@@ -4,11 +4,6 @@ import com.nhlstenden.student.vigmo.models.Screen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ScreenRepository extends JpaRepository<Screen, Long> {
-
-    @Override
-    Optional<Screen> findById(Long aLong);
 }
