3f3a19fbb282ffb9759085bc86453c5d66ce7f17
Brandon <43607839+brann0n@users.noreply.github.com>
2021-12-20 19:36:24 +0100

    added documentation and a landingspage

--- src/main/java/com/nhlstenden/student/vigmo/config/SwaggerConfig.java
+++ src/main/java/com/nhlstenden/student/vigmo/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.nhlstenden.student.vigmo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,6 +17,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -27,5 +31,14 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Vigmo API")
+                .description("Vigmo API is the backend api for an Announcements board at NHL Stenden. Made by Students.")
+                .license("MIT")
+                .version("1.0.0")
+                .build();
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Brandon
+  Date: 20/12/2021
+  Time: 19:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Vigmo Api</title>
+</head>
+<body style="font-family: Open Sans,sans-serif;">
+    <div style="height: 300px; width: 300px; margin: auto; position: absolute; top: 0; left: 0; right: 0; bottom: 0;">
+        <h1 style="text-align: center;">Vigmo API</h1>
+        <a href="/swagger-ui.html" style="width: 100%; display: block; text-align: center;">Go to Swagger</a>
+    </div>
+</body>
+</html>
