fc1d072f2fd6c8f862c3e1958b8dd9244e445da0
Brandon <43607839+brann0n@users.noreply.github.com>
2021-12-21 13:58:44 +0100

    Made the controller abstract too

---
+++ src/main/java/com/nhlstenden/student/vigmo/controllers/AbstractVigmoController.java
@@ -0,0 +1,55 @@
+package com.nhlstenden.student.vigmo.controllers;
+
+import com.nhlstenden.student.vigmo.services.VigmoService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+import java.util.List;
+
+public abstract class AbstractVigmoController<Service extends VigmoService<DTO>, DTO> implements VigmoController<DTO> {
+
+    private final Service service;
+
+    public AbstractVigmoController(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    @ApiOperation(value = "Gets list of objects in the database")
+    public ResponseEntity<List<DTO>> get() {
+        return ResponseEntity.ok(service.getList());
+    }
+
+    @ApiOperation(value = "Gets a specific object")
+    @Override
+    public ResponseEntity<DTO> get(final long id) {
+        return ResponseEntity.ok(service.get(id));
+    }
+
+    @ApiOperation(value = "Creates a new object")
+    @Override
+    public ResponseEntity<Void> post(DTO postObject) {
+        return ResponseEntity.created(URI.create(String.format("/%s/%d", getPathName(), service.create(postObject)))).build();
+    }
+
+    @ApiOperation(value = "Updates an existing object in the database")
+    @Override
+    public ResponseEntity<Void> put(final long id, DTO putObject) {
+        service.update(putObject, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation(value = "Deletes a object from the database")
+    @Override
+    public ResponseEntity<Void> delete(final long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    private String getPathName() {
+        String[] annotationValues = getClass().getAnnotation(RequestMapping.class).value();
+        return String.join("", annotationValues);
+    }
+}
--- src/main/java/com/nhlstenden/student/vigmo/controllers/ConsultationHourController.java
+++ src/main/java/com/nhlstenden/student/vigmo/controllers/ConsultationHourController.java
@@ -1,47 +1,17 @@
 package com.nhlstenden.student.vigmo.controllers;
 
-import com.nhlstenden.student.vigmo.dto.ConsultationHourDto;
-import com.nhlstenden.student.vigmo.services.ConsultationHourService;
+import com.nhlstenden.student.vigmo.dto.ScreenDto;
+import com.nhlstenden.student.vigmo.services.ScreenService;
 import io.swagger.annotations.Api;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Api("Consultation Hour Controller")
 @RestController
 @RequestMapping("consultation_hour")
-public class ConsultationHourController implements VigmoController<ConsultationHourDto> {
+public class ConsultationHourController extends AbstractVigmoController<ScreenService, ScreenDto> {
 
-    private final ConsultationHourService service;
-
-    public ConsultationHourController(ConsultationHourService service){
-        this.service = service;
-    }
-
-    @Override
-    public ResponseEntity<List<ConsultationHourDto>> get() {
-        return ResponseEntity.ok(service.getList());
-    }
-
-    @Override
-    public ResponseEntity<ConsultationHourDto> get(long id) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> post(ConsultationHourDto postObject) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> put(long id, ConsultationHourDto putObject) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> delete(long id) {
-        return null;
+    public ConsultationHourController(ScreenService service) {
+        super(service);
     }
 }
