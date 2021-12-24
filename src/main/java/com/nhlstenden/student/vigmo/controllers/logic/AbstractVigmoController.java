b6d29d46e0443213cd8e4fafd1f42375bf41a854
MarkCorazon <markvanderhart1999@gmail.com>
2021-12-24 14:07:27 +0100

    Added Location as an exposed header

--- src/main/java/com/nhlstenden/student/vigmo/controllers/logic/AbstractVigmoController.java
+++ src/main/java/com/nhlstenden/student/vigmo/controllers/logic/AbstractVigmoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.URI;
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(exposedHeaders = "X-Total-Count, Location")
 public abstract class AbstractVigmoController<Service extends VigmoService<DTO>, DTO> implements VigmoController<DTO> {
 
     private final Service service;
