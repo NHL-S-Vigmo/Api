88f16aa55fbd2957219fb1913cd6ef15c4577762
Brandon <43607839+brann0n@users.noreply.github.com>
2021-12-24 11:43:27 +0100

    Added a nice header thing

--- src/main/java/com/nhlstenden/student/vigmo/controllers/advice/ResourceSizeHandler.java
+++ src/main/java/com/nhlstenden/student/vigmo/controllers/advice/ResourceSizeHandler.java
@@ -2,12 +2,15 @@ package com.nhlstenden.student.vigmo.controllers.advice;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
 import java.util.List;
 
 @RestControllerAdvice
@@ -17,11 +20,16 @@ public class ResourceSizeHandler implements ResponseBodyAdvice<List<?>> {
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         //Checks if this advice is applicable.
         //In this case it applies to any endpoint which returns a collection.
-        return List.class.isAssignableFrom(returnType.getParameterType());
+
+        String typeName = ((ParameterizedType)returnType.getGenericParameterType()).getActualTypeArguments()[0].getTypeName();
+        return typeName.contains("java.util.List");
     }
 
     @Override
-    public List<?> beforeBodyWrite(List<?> body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+    public List<?> beforeBodyWrite(List<?> body, MethodParameter returnType, MediaType selectedContentType,
+                                         Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                         ServerHttpRequest request,
+                                         ServerHttpResponse response) {
         response.getHeaders().add("X-Total-Count", String.valueOf(body.size()));
         return body;
     }
