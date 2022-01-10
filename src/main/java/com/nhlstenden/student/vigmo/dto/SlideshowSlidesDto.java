423751f17b1934f067ccfe39d975cc6da5430bc3
Brandon <43607839+brann0n@users.noreply.github.com>
2022-01-10 20:15:01 +0100

    getting api Data from slideshows now works.

--- src/main/java/com/nhlstenden/student/vigmo/dto/SlideshowSlidesDto.java
+++ src/main/java/com/nhlstenden/student/vigmo/dto/SlideshowSlidesDto.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SlideshowSlidesDto {
-    private Long slideshowId;
+    private Long slideId;
     private Boolean isActive;
     private Integer duration;
     private String path; // /media_slides
--- src/main/java/com/nhlstenden/student/vigmo/services/SlideshowService.java
+++ src/main/java/com/nhlstenden/student/vigmo/services/SlideshowService.java
@@ -7,6 +7,7 @@ import com.nhlstenden.student.vigmo.exception.DataNotFoundException;
 import com.nhlstenden.student.vigmo.models.Slide;
 import com.nhlstenden.student.vigmo.models.Slideshow;
 import com.nhlstenden.student.vigmo.models.SlideshowVariable;
+import com.nhlstenden.student.vigmo.models.TextSlide;
 import com.nhlstenden.student.vigmo.repositories.SlideshowRepository;
 import com.nhlstenden.student.vigmo.services.logic.AbstractVigmoService;
 import com.nhlstenden.student.vigmo.transformers.MappingUtility;
@@ -17,6 +18,7 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class SlideshowService extends AbstractVigmoService<SlideshowRepository, SlideshowDto, Slideshow> {
@@ -36,6 +38,18 @@ public class SlideshowService extends AbstractVigmoService<SlideshowRepository, 
         List<SlideshowSlidesDto> returnList = new ArrayList<>();
         Slideshow slideshow = repo.getById(id);
         List<Slide> slides = slideshow.getSlideList();
+
+        //go through each slide and add details about it.
+        for(Slide slide: slides){
+            SlideshowSlidesDto slideDto = new SlideshowSlidesDto();
+            slideDto.setSlideId(slide.getId());
+            slideDto.setDuration(slide.getDuration());
+            slideDto.setIsActive(slide.getIsActive());
+
+            String pathName = slide.getClass().getSimpleName().toLowerCase(Locale.ROOT).replace("slide", "_slides");
+            slideDto.setPath(String.format("/%s/%d", pathName, slide.getId()));
+            returnList.add(slideDto);
+        }
 
         return returnList;
     }
