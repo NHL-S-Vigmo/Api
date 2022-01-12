fa4268c52c5a1db03a39656e9c180d17448713dc
Brandon <43607839+brann0n@users.noreply.github.com>
2022-01-12 16:35:20 +0100

    Added tests and fixed some bugs

--- src/main/java/com/nhlstenden/student/vigmo/controllers/security/AuthenticateController.java
+++ src/main/java/com/nhlstenden/student/vigmo/controllers/security/AuthenticateController.java
@@ -3,6 +3,9 @@ package com.nhlstenden.student.vigmo.controllers.security;
 import com.nhlstenden.student.vigmo.security.JWTProvider;
 import com.nhlstenden.student.vigmo.security.models.LoginDto;
 import com.nhlstenden.student.vigmo.services.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -25,6 +28,12 @@ public class AuthenticateController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "Endpoint used for signing in as a user.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Signed in, jwt is in header"),
+            @ApiResponse(code = 401, message = "Invalid credentials"),
+            @ApiResponse(code = 403, message = "Account is disabled")
+    })
     @PostMapping
     public ResponseEntity<Void> login(@RequestBody LoginDto login) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword()));
