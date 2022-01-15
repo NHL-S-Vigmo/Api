c4948b6c2bd312cf045c1044da7b6eae91dc6caa
Brandon <43607839+brann0n@users.noreply.github.com>
2022-01-15 15:30:18 +0100

    fixed the broken test

--- src/test/java/com/nhlstenden/student/vigmo/services/SlideshowServiceTest.java
+++ src/test/java/com/nhlstenden/student/vigmo/services/SlideshowServiceTest.java
@@ -1,20 +1,17 @@
 package com.nhlstenden.student.vigmo.services;
 
-import com.nhlstenden.student.vigmo.dto.SlideshowDto;
 import com.nhlstenden.student.vigmo.dto.SlideshowVariableDto;
-import com.nhlstenden.student.vigmo.dto.UserDto;
 import com.nhlstenden.student.vigmo.models.Slide;
 import com.nhlstenden.student.vigmo.models.Slideshow;
-import com.nhlstenden.student.vigmo.models.User;
+import com.nhlstenden.student.vigmo.models.SlideshowVariable;
 import com.nhlstenden.student.vigmo.repositories.SlideshowRepository;
 import com.nhlstenden.student.vigmo.transformers.MappingUtility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,16 +30,18 @@ class SlideshowServiceTest {
     Slideshow slideshowEntityMock;
 
     @Mock
+    SlideshowVariable slideshowVariableEntityMock;
+
+    @Mock
+    SlideshowVariableDto slideshowVariableDtoMock;
+
+    @Mock
     private MappingUtility mapper;
 
     @InjectMocks
     SlideshowService slideshowService;
 
-    @Captor
-    private ArgumentCaptor<List<SlideshowVariableDto>> slidesDtoCaptor;
 
-    @Captor
-    private ArgumentCaptor<List<Slide>> slidesCaptor;
 
     @BeforeEach
     void setUp() {
@@ -51,20 +50,23 @@ class SlideshowServiceTest {
 
     @Test
     void getVariables() {
+        List<SlideshowVariableDto> slideshowVariableDtosMock = Arrays.asList(slideshowVariableDtoMock, slideshowVariableDtoMock, slideshowVariableDtoMock);
+        List<SlideshowVariable> slideshowVariablesMock = Arrays.asList(slideshowVariableEntityMock, slideshowVariableEntityMock, slideshowVariableEntityMock);
+
         when(slideshowRepositoryMock.findById(anyLong())).thenReturn(Optional.of(slideshowEntityMock));
-        when(mapper.mapList(slidesCaptor.capture(), eq(SlideshowVariableDto.class))).thenReturn(slidesDtoCaptor.capture());
+        when(mapper.mapList(eq(slideshowVariablesMock), eq(SlideshowVariableDto.class))).thenReturn(slideshowVariableDtosMock);
+        when(slideshowEntityMock.getSlideshowVariableList()).thenReturn(slideshowVariablesMock);
 
         List<SlideshowVariableDto> variables = slideshowService.getVariables(1L);
 
         //check if there is actually something returned.
-        assertThat(variables).isEqualTo(slidesDtoCaptor.getValue());
+        assertThat(variables).isEqualTo(slideshowVariableDtosMock);
 
         //verify that the find and getSlideShowVariables functions were called
         verify(slideshowRepositoryMock).findById(anyLong());
         verify(slideshowEntityMock).getSlideshowVariableList();
 
-        verify(mapper).mapObject(slidesCaptor.capture(), any());
-        //fixme: needs completions
+        verify(mapper).mapList(eq(slideshowVariablesMock), any());
     }
 
     @Test
